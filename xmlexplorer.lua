13290d1b73e8bd469bb8ba0fbfd5118e4b2790e3
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2022-01-15 09:04:02 +0200

    xmlexplorer: Omit the root and sort attributes before tags

--- xmlexplorer.lua
+++ xmlexplorer.lua
@@ -59,12 +59,24 @@ for filename in fs.scandirSync(dir) do
 	end
 end
 
+local function sortfunc(a, b)
+	local aa = string.sub(names[a], 1, 1) == "@"
+	local bb = string.sub(names[b], 1, 1) == "@"
+	if aa ~= bb then
+		return aa
+	else
+		return counts[a] > counts[b]
+	end
+end
+
 local function prn(tbl, name, level)
-	table.sort(tbl, function(a, b) return counts[a] > counts[b] end)
-	io.write(string.rep("\t", level) .. name .. " " .. (counts[tbl] or "X") .. "\n")
+	table.sort(tbl, sortfunc)
+	io.write(string.rep("\t", level) .. name .. " " .. counts[tbl] .. "\n")
 	for i,v in ipairs(tbl) do
 		prn(v, names[v], level + 1)
 	end
 end
 
-prn(root, "root", 0)
+for i,v in ipairs(root) do
+	prn(v, names[v], 0)
+end
